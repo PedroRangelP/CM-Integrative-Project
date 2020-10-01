8dc0c2a141c49bb203ee92369eff2032f11a1b54
Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>
2020-10-01 18:43:28 -0500

    DFA methods
    Co-Authored-By: DiegoMont <31603020+DiegoMont@users.noreply.github.com>

--- Part1/DFA.java
+++ Part1/DFA.java
@@ -1,21 +1,26 @@
+import java.util.HashTable;
+
 public class DFA {
-    private State[] setStates;
+    private HashTable<String, State> setStates;
     private String[] alphabet;
     private String initialState;
-    private String[] finalState;
+    private String[] setFinalStates;
     
     public DFA(String[] states, String[] alphabet, String initialState, String[] finalState) {
-        setStates = new State[states.lenght];
-        for (int i=0; i<states.lenght; i++) {
-            setStates[i] = 
+        setStates = new HashTable<>();
+        for (int i=0; i<states.length; i++) {
+            setStates.put(states[i], new State(states[i]));
         }
 
-        this.alphabet = alphabet;
         this.initialState = initialState;
-        this.finalState = finalState;
+
+
+        for (int i=0; i<finalState.length; i++) {
+            setStates.get(finalState[i]).setFinal();
+        }
     }
 
     public void addTransition(String startState, String letter, String endState) {
-
+        setStates.get(startState).setTransition(setStates.get(endState), letter);
     }
 }
--- Part1/Main.java
+++ Part1/Main.java
@@ -10,18 +10,14 @@ public class Main {
         String initialState = sc.next(); sc.nextLine();
         String[] finalStates = sc.nextLine().split(",");
 
-        //DFA automata = new DFA(states, characters, initialState, finalStates);
+        DFA automata = new DFA(states, characters, initialState, finalStates);
 
         while(sc.hasNextLine()){
             String[] transition = sc.nextLine().split(",");
             String[] charYState = transition[1].split("=>");
-            addTransition(transition[0], charYState[0], charYState[1]);
+            automata.addTransition(transition[0], charYState[0], charYState[1]);
         }
 
         sc.close();
-    }
-
-    public static void addTransition(String startState, String letter, String endState) {
-        System.out.println(String.format("%s   %s   %s", startState, letter, endState));
     }
 }
