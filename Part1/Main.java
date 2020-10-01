a1da3f4e74bce1f5e14d6baf6ec1df99cc4e66bd
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2020-10-01 18:02:42 -0500

    Leer input para crear automata

--- Part1/Main.java
+++ Part1/Main.java
@@ -9,12 +9,19 @@ public class Main {
         String[] characters = sc.nextLine().split(",");
         String initialState = sc.next(); sc.nextLine();
         String[] finalStates = sc.nextLine().split(",");
-        while(sc.hasNextLine()){
-            String[] transitionRaw = sc.nextLine().clip();
-            String[] 
-        }
-        DFA automata = new DFA();
 
-        sc.close()
+        //DFA automata = new DFA(states, characters, initialState, finalStates);
+
+        while(sc.hasNextLine()){
+            String[] transition = sc.nextLine().split(",");
+            String[] charYState = transition[1].split("=>");
+            addTransition(transition[0], charYState[0], charYState[1]);
+        }
+
+        sc.close();
+    }
+
+    public static void addTransition(String startState, String letter, String endState) {
+        System.out.println(String.format("%s   %s   %s", startState, letter, endState));
     }
 }
